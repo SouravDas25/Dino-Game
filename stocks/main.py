c462b3788f5810c31816a5dec6e82e34b656d28e
SilentSamurai <souravbumbadas25@gmail.com>
2023-07-02 21:02:57 +0530

    updated

---
+++ stocks/main.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+# %matplotlib inline
+
+from matplotlib.pylab import rcParams
+
+rcParams['figure.figsize'] = 20, 10
+from keras.models import Sequential
+from keras.layers import LSTM, Dropout, Dense
+
+from sklearn.preprocessing import MinMaxScaler
+
+df = pd.read_csv("archive/ADANIPORTS.csv")
+df.head()
+
+df["Date"] = pd.to_datetime(df.Date, format="%Y-%m-%d")
+df.index = df['Date']
+plt.figure(figsize=(16, 8))
+plt.plot(df["Close"], label='Close Price history')
